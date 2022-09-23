5cd1e060a6784265d08e70ce5d107ee8f7788cda
DESKTOP\adona65 <adona65>
2022-09-23 11:32:45 +0200

    - Add tutorials about unit tests with spring boot.

---
+++ src/test/java/tutorial/springboot/HelloWorldControllerTest.java
@@ -0,0 +1,32 @@
+package tutorial.springboot;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * The embedded server starts on a random port because of 'webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT", and the actual port is 
+ * configured automatically in the base URL for the TestRestTemplate.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloWorldControllerTest {
+
+	@Autowired
+	private TestRestTemplate template;
+
+	/**
+	 * Instead of mocking the HTTP request cycle, as in SpringbootApplicationTests.getHello(), we also can use Spring Boot to write 
+	 * a simple full-stack integration test. For example, instead of (or as well as) the mock used in getHello(), we could create the 
+	 * bellow test.
+	 */
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/", String.class);
+        assertThat(response.getBody()).isEqualTo("Welcome to default page!");
+    }
+}
--- src/test/java/tutorial/springboot/SpringbootApplicationTests.java
+++ src/test/java/tutorial/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,40 @@
 package tutorial.springboot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+/**
+ * Using correct annotations, Spring allow us to write unit tests on our application :<br/>
+ * - @SpringBootTest indicate a test class that runs Spring Boot based tests. This annotation ask for the whole application context to 
+ * 		be created. An alternative would be to ask Spring Boot to create only the web layers of the context by using @WebMvcTest<br/>
+ * - @AutoConfigureMockMvc allow to inject a MockMvc instance. MockMvc comes from Spring Test and lets, through a set of convenient 
+ * 		builder classes, send HTTP requests into the DispatcherServlet and make assertions about the result.
+ */
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringbootApplicationTests {
 
+	/**
+	 * @Autowired marks a constructor, field, setter method, or config method as to be autowired by Spring's dependency injection 
+	 * facilities. This MockMvc object will be injected thanks to @AutoConfigureMockMvc annotations put on the class.<br/>
+	 * In this method, we mock the HTTP request cycle.
+	 */
+	@Autowired
+	private MockMvc mvc;
+	
 	@Test
-	void contextLoads() {
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Welcome to default page!")));
 	}
-
 }
